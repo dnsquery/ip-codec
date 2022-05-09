bd533978831f3c52e3a6e5d92cb1fe46769ceff2
Martin Heidegger <martin.heidegger@gmail.com>
2022-05-10 01:41:25 +0900

    ci: windows support

--- scripts/esm2umd.mjs
+++ scripts/esm2umd.mjs
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
-
-import fs from 'fs'
+import fs from 'fs/promises'
 import esm2umd from 'esm2umd'
 
-if (process.argv.length < 4) {
-  console.error('Usage: esm2umd ModuleName esmFile.js > umdFile.js')
-  process.exit(1)
-}
-
-process.stdout.write(esm2umd(process.argv[2], fs.readFileSync(process.argv[3]), { importInterop: 'node' }))
+;(async () => {
+  const dirname = new URL('..', import.meta.url)
+  const files = await fs.readdir(dirname)
+  for (const file of files.filter(file => file.endsWith('.mjs'))) {
+    const name = file.replace(/\.mjs$/, '')
+    const esmFile = await fs.readFile(new URL(file, dirname))
+    const target = `${name}.cjs`
+    await fs.writeFile(
+      new URL(target, dirname),
+      esm2umd(process.argv[2], esmFile, { importInterop: 'node' })
+    )
+  }
+})()
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
