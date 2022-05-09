215a156771b82b8ff03f167b8bf1f654c6477b18
Martin Heidegger <martin.heidegger@gmail.com>
2022-05-10 01:48:09 +0900

    ci: node 12 fix

--- scripts/esm2umd.mjs
+++ scripts/esm2umd.mjs
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import fs from 'fs/promises'
+import fs from 'fs'
 import esm2umd from 'esm2umd'
 
 ;(async () => {
   const dirname = new URL('..', import.meta.url)
-  const files = await fs.readdir(dirname)
+  const files = await fs.promises.readdir(dirname)
   for (const file of files.filter(file => file.endsWith('.mjs'))) {
     const name = file.replace(/\.mjs$/, '')
-    const esmFile = await fs.readFile(new URL(file, dirname))
+    const esmFile = await fs.promises.readFile(new URL(file, dirname))
     const target = `${name}.cjs`
-    await fs.writeFile(
+    await fs.promises.writeFile(
       new URL(target, dirname),
       esm2umd(process.argv[2], esmFile, { importInterop: 'node' })
     )
